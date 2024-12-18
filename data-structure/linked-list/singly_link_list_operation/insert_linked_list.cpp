e13320aaf60d5264193e3fe796a8ecfcebf9e4ac
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2024-12-18 08:53:05 -0800

    singly liked list insert

--- data-structure/linked-list/singly_link_list_operation/insert_linked_list.cpp
+++ data-structure/linked-list/singly_link_list_operation/insert_linked_list.cpp
@@ -32,9 +32,34 @@ public:
     }
 };
 
+// insert at head in linked list
+void insert_at_head(Node *head, int val)
+{
+
+    Node *new_node = new Node(val);
+    new_node->next = head;
+};
+
 // main fuction
 int main()
 {
-    cout << "hello world";
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+
+    head->next = a;
+    a->next = b;
+
+    // insert in head
+    insert_at_head(head, 300);
+
+    Node *temp = head;
+
+    while (temp->next != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+
     return 0;
 }
